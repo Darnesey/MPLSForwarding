0bd5d3f7a8067e4b6cac88931266ca2f4f46d93d
Darnesey <rdarnell1992@gmail.com>
2016-12-07 00:41:03 -0700

    Built new topology and setup basic structure for MPLS routing tables. This may need some tweeking but it's a start.

--- src-part2/simulation.py
+++ src-part2/simulation.py
@@ -8,6 +8,7 @@ import link
 import threading
 from time import sleep
 import sys
+INF = 9999999
 
 ##configuration parameters
 router_queue_size = 0 #0 means unlimited
@@ -15,37 +16,67 @@ simulation_time = 20 #give the network sufficient time to transfer all packets b
 
 if __name__ == '__main__':
     object_L = [] #keeps track of objects, so we can kill their threads
-    
-    #create network hosts
-    client = network.Host(1)
-    object_L.append(client)
-    server = network.Host(2)
-    object_L.append(server)
-    
-    #create routers and routing tables for connected clients (subnets)
-    router_a_rt_tbl_D = {1: {0: 1}} # packet to host 1 through interface 0 for cost 1
-    router_a = network.Router(name='A', 
-                              intf_cost_L=[1,1], 
-                              intf_capacity_L=[500,500],
-                              mpls_table = router_a_rt_tbl_D,
-                              max_queue_size = router_queue_size)
+
+    # create network hosts
+    host_1 = network.Host(1)
+    object_L.append(host_1)
+    host_2 = network.Host(2)
+    object_L.append(host_2)
+    host_3 = network.Host(2)
+    object_L.append(host_3)
+
+    # create routers and routing tables for connected clients (subnets)
+    router_a_mpls_tbl = {1: [0, 10, 2], 2: [1, 20, 3]} # This table is confusing as there would not be MPLS frames on the incoming packets. I'm not sure what the keys should be
+    router_a = network.Router(name='A',
+                              intf_cost_L=[1, 1, 1, 1],
+                              intf_capacity_L=[500,500,500,500],
+                              mpls_table=router_a_mpls_tbl,
+                              max_queue_size=router_queue_size)
     object_L.append(router_a)
-    router_b_rt_tbl_D = {2: {1: 3}} # packet to host 2 through interface 1 for cost 3
-    router_b = network.Router(name='B', 
-                              intf_cost_L=[1,3], 
-                              intf_capacity_L=[500,100],
-                              mpls_table = router_b_rt_tbl_D,
-                              max_queue_size = router_queue_size)
+    router_b_mpls_tbl = {10: [0,15,1]} #In Label: 10 with attributes in the form [in Interface, out Label, out Interface]
+    router_b = network.Router(name='B',
+                              intf_cost_L=[1, 2],
+                              intf_capacity_L=[500,500],
+                              mpls_table=router_b_mpls_tbl,
+                              max_queue_size=router_queue_size)
     object_L.append(router_b)
-    
-    #create a Link Layer to keep track of links between network nodes
+    router_c_mpls_tbl = {20: [0,25,1]}
+    router_c = network.Router(name='C',
+                              intf_cost_L=[1, 1],
+                              intf_capacity_L=[500,500],
+                              mpls_table=router_c_mpls_tbl,
+                              max_queue_size=router_queue_size)
+    object_L.append(router_c)
+    router_d_mpls_tbl = {15: [1,"POP",0], 25: [2,"POP",0]} # Whenever POP is seen, pop the MPLS frame from the network packet
+    router_d = network.Router(name='D',
+                              intf_cost_L=[1, 1, 2],
+                              intf_capacity_L=[100,500,500],
+                              mpls_table=router_d_mpls_tbl,
+                              max_queue_size=router_queue_size)
+    object_L.append(router_d)
+    # create a Link Layer to keep track of links between network nodes
     link_layer = link.LinkLayer()
     object_L.append(link_layer)
-    
-    #add all the links
-    link_layer.add_link(link.Link(client, 0, router_a, 0))
-    link_layer.add_link(link.Link(router_a, 1, router_b, 0))
-    link_layer.add_link(link.Link(router_b, 1, server, 0))
+
+    # add all the links
+    # host_1
+    link_layer.add_link(link.Link(host_1, 0, router_a, 0))
+
+    # host_2
+    link_layer.add_link(link.Link(host_2, 0, router_a, 1))
+
+    # host3
+    link_layer.add_link(link.Link(host_3, 0, router_d, 0))
+
+    # router_a
+    link_layer.add_link(link.Link(router_a, 2, router_b, 0))
+    link_layer.add_link(link.Link(router_a, 3, router_c, 0))
+
+    # router_b
+    link_layer.add_link(link.Link(router_b, 1, router_d, 1))
+
+    # router_c
+    link_layer.add_link(link.Link(router_c, 1, router_d, 2))
     
     
     #start all the objects
